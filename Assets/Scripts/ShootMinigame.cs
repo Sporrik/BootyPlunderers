c7ad632848290e64e225d0c33d23f5c67dc9695f
milasmyk <milena.smyk.be@gmail.com>
2024-05-19 18:56:18 +0200

    minigame

--- Assets/Scripts/ShootMinigame.cs
+++ Assets/Scripts/ShootMinigame.cs
@@ -1,32 +1,80 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootMinigame : MonoBehaviour
 {
-    public Collider2D miss, hit, bullseye, pointer;
+    public Collider2D miss, miss2, hit, hit2, bullseye, pointer;
+
+    public int enemyHealth, hitDamage, bullseyeDamage, shotCount, maxShots;
+    private int _speed = 1;
+
+    public Slider enemyHealthSlider;
+    public Image sliderFill;
+    public UnityEngine.Color green, yellow, red;
+
+    private void Start()
+    {        
+        enemyHealth = 100;
+        enemyHealthSlider.maxValue = 100;
+        enemyHealthSlider.value = enemyHealth;
+        maxShots = 10; //would be the amount of bullets?
+        hitDamage = 10;
+        bullseyeDamage = 15;
+    }
 
     void Update()
     {
-        pointer.gameObject.transform.position = new Vector3 (Mathf.Sin(Time.time) * 5, 4f, -3f);
-        
-        CheckCollision();
+        if (shotCount < maxShots)
+        {
+            pointer.gameObject.transform.position = new Vector3(Mathf.Sin(Time.time * _speed) * 5, 4f, -3f); //*5 for distance
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CheckCollision();
+                shotCount++;                
+            }
+        }               
+    }
+
+    private void UpdateSlider(int health)
+    {
+        enemyHealthSlider.value = health;
+        if (health > 50)
+        {
+            sliderFill.color = green;
+        }
+        else if (health <= 50 && health > 26)
+        {
+            sliderFill.color = yellow;
+        }
+        else if (health <= 25)
+        {
+            sliderFill.color = red;
+        }
     }
 
     private void CheckCollision()
     {
-        if (pointer.IsTouching(miss))
-        {
-            Debug.Log("Miss!");
+        if (pointer.IsTouching(miss) || pointer.IsTouching(miss2))
+        {            
+            Debug.Log($"Miss! Enemy health: {enemyHealth}");
+            UpdateSlider(enemyHealth);
         }
-        else if (pointer.IsTouching(hit))
+        else if (pointer.IsTouching(hit) || pointer.IsTouching(hit2))
         {
-            Debug.Log("Hit!");
+            enemyHealth -= hitDamage;
+            Debug.Log($"Hit! Enemy health: {enemyHealth}");
+            UpdateSlider(enemyHealth);
         }
         else if (pointer.IsTouching(bullseye))
         {
-            Debug.Log("Bullseye!");
+            enemyHealth -= bullseyeDamage;
+            Debug.Log($"Bullseye! Enemy health: {enemyHealth}");
+            UpdateSlider(enemyHealth);
         }
     }
 }
