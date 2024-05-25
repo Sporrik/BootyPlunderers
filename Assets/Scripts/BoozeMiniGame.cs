5d44023ecfb34f74b45f4c8b7950e0d98d5f045c
milasmyk <milena.smyk.be@gmail.com>
2024-05-25 22:41:27 +0200

    BOOZE MINIGAME DONE

--- Assets/Scripts/BoozeMiniGame.cs
+++ Assets/Scripts/BoozeMiniGame.cs
@@ -6,21 +6,32 @@ using UnityEngine;
 
 public class BoozeMiniGame : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
+
     public GameObject bottlePrefab, bombPrefab;
     private Vector3 _bottomLeftCorner, _topRightCorner;
 
-    [SerializeField]
-    private float _maxSpeed = 10f,
-                  _minSpeed = 5f;
-
-    private int _maxBottlesInScene = 10,
-                _maxBombsInScene = 7,
-                _bottlesInScene,
+    private int _maxBottlesInScene = 15, _score, _scorePerBottle = 5, _timeOfRound = 10,
+                _maxBombsInScene = 10;
+    private int _bottlesInScene,
                 _bombsInScene;
+
     public List<GameObject> bottleList, bombList;
+
+    private GameObject _heldObject;
+
+    [SerializeField] Collider2D pirateMouth;
+
+    [SerializeField] TextMeshProUGUI scoreText, timerText, finalScoreText;
+    [SerializeField] GameObject gameOverPanel;
+
+    private float _bombTimer, _timer;
+    private bool _isCounting = true;
 
     void Start()
     {
+        _timer = _timeOfRound;
+        DisplayScore();
         bottleList = new List<GameObject>();
         bombList = new List<GameObject>();
 
@@ -35,6 +46,103 @@ public class BoozeMiniGame : MonoBehaviour
         {
             SpawnBombs();
         }
+    }
+
+    private void Update()
+    {
+        if (_isCounting)
+        {
+            _timer -= Time.deltaTime;
+            timerText.text = Mathf.Ceil(_timer).ToString();
+        }
+        if (_timer > 0)
+        {
+            DisplayScore();
+            CheckMouseCollision();
+            CheckCollisionObject();
+        }
+        else if (_timer <= _timeOfRound )
+        {
+            _isCounting = false;
+            EndGame();
+        }
+    }
+
+    private void EndGame()
+    {
+        gameOverPanel.gameObject.SetActive(true);
+        _heldObject.GetComponent<BoozeMiniGameMovement>().isMoving = true;
+        if (_score < 0)
+        {
+            finalScoreText.text = $"0 HP";
+        }
+        else
+        {
+            finalScoreText.text = $"{_score} HP";
+        }        
+    }
+
+    private void CheckCollisionObject()
+    {
+        if (_heldObject && _heldObject.CompareTag("Booze"))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveHeldObject();
+            }
+            else if (_heldObject.GetComponent<Collider2D>().IsTouching(pirateMouth) && Input.GetMouseButtonUp(0))
+            {
+                _score += _scorePerBottle;
+                Destroy(_heldObject);
+                _heldObject = null;
+            }
+            else
+            {
+                _heldObject.GetComponent<BoozeMiniGameMovement>().isMoving = true;
+                _heldObject = null;
+            }
+        }
+        else if (_heldObject && _heldObject.CompareTag("Bomb"))
+        {
+            _bombTimer += Time.deltaTime;
+            _heldObject.GetComponent<SpriteRenderer>().color = Color.red;
+
+            if (_bombTimer >= .2f)
+            {
+                _score -= _scorePerBottle;                
+                Destroy(_heldObject);
+                _heldObject = null;
+            }
+        }
+    }
+
+    private void DisplayScore()
+    {        
+        scoreText.text = $"{_score}";
+    }
+
+    private void CheckMouseCollision()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider)
+            {
+                if (hit.collider.TryGetComponent<BoozeMiniGameMovement>(out BoozeMiniGameMovement movementScript))
+                {
+                    movementScript.isMoving = false;
+                    _heldObject = hit.collider.gameObject;
+                }                
+            }
+        }
+    }
+
+    private void MoveHeldObject()
+    {
+        var cameraPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        _heldObject.transform.position = new Vector3(cameraPosition.x,cameraPosition.y, transform.position.z);
     }
 
     private void SpawnBombs()
@@ -55,43 +163,5 @@ public class BoozeMiniGame : MonoBehaviour
         var bottle = Instantiate(bottlePrefab);
         _bottlesInScene++;
         bottleList.Add(bottle);
-    }
-
-    void Update()
-    {
-        MoveObjects(bottleList);
-        MoveObjects(bombList);
-    }
-
-    private void MoveObjects(List<GameObject> list)
-    {
-        foreach (GameObject obj in list)
-        {
-            obj.transform.position += GetVelocity() * Time.deltaTime; // the get velocity is the problem
-        }
-    }
-
-    public Vector3 GetVelocity()
-    {
-        Vector3 direction = GetRandomDirection(); //THIS IS THE PROBLEM
-        float speed = GetRandomSpeed(); //ALSO THIS
-        var velocity = direction * speed;
-        return velocity;
-    }
-
-    private Vector3 GetRandomDirection()
-    {
-        Vector3 direction;
-        direction.x = UnityEngine.Random.Range(-1.0f, 1.0f);
-        direction.y = UnityEngine.Random.Range(-1.0f, 1.0f);
-        direction.z = 0;
-        return direction;
-    }
-
-    private float GetRandomSpeed()
-    {
-        float speed;
-        speed = UnityEngine.Random.Range(_minSpeed, _maxSpeed);
-        return speed;
     }
 }
---
+++ Assets/Scripts/BoozeMiniGameMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoozeMiniGameMovement : MonoBehaviour
+{
+    private float _speed, _minSpeed = 10, _maxSpeed = 15;
+    private Vector3 _direction, _velocity;
+    public bool isMoving;
+    private Vector3 _bottomLeftCorner, _topRightCorner;    
+
+    private void Start()
+    {
+        _bottomLeftCorner = new Vector3(-9, -5, 0);
+        _topRightCorner = new Vector3(9, 5, 0);
+        isMoving = true;
+        _velocity = GetVelocity();        
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.position += _velocity * Time.deltaTime;
+            GoingOffScreen();
+        }
+    }
+
+    public Vector3 GetVelocity()
+    {
+        _direction = GetRandomDirection();
+        _speed = GetRandomSpeed();
+        var velocity = _direction * _speed;
+        return velocity;
+    }
+
+    private Vector3 GetRandomDirection()
+    {
+        Vector3 direction;
+        direction.x = UnityEngine.Random.Range(-1.0f, 1.0f);
+        direction.y = UnityEngine.Random.Range(-1.0f, 1.0f);
+        direction.z = 0;
+        return direction;
+    }
+
+    private float GetRandomSpeed()
+    {
+        float speed;
+        speed = UnityEngine.Random.Range(_minSpeed, _maxSpeed);
+        return speed;
+    }
+
+    private void GoingOffScreen()
+    {
+        if (transform.position.x < _bottomLeftCorner.x - 1)
+        {
+            transform.position = new Vector3(_topRightCorner.x + 1, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > _topRightCorner.x + 1)
+        {
+            transform.position = new Vector3(_bottomLeftCorner.x - 1, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y < _bottomLeftCorner.y - 1)
+        {
+            transform.position = new Vector3(transform.position.x, _topRightCorner.y + 1, transform.position.z);
+        }
+
+        if (transform.position.y > _topRightCorner.y + 1)
+        {
+            transform.position = new Vector3(transform.position.x, _bottomLeftCorner.y - 1, transform.position.z);
+        }
+    }
+}
