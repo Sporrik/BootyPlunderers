09afd5ffe3265c145e56d809fed45e0fae9d3320
Belbs15 <glassir369@gmail.com>
2024-05-27 11:00:56 +0200

    Parrot mini game - Cleaning up code and fixing the gap between pillars
    The gap between pillars not changing height.

--- Assets/Scripts/ParrotMinigame/Pillar.cs
+++ Assets/Scripts/ParrotMinigame/Pillar.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class Pillar : MonoBehaviour
 {
     public bool _TouchedPlayer = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
