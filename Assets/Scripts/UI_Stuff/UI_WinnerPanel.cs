12b034495fd2437327e0b2a370d34d28b73b83e8
milasmyk <milena.smyk.be@gmail.com>
2024-05-29 14:29:25 +0200

    lil fix

--- Assets/Scripts/UI_Stuff/UI_WinnerPanel.cs
+++ Assets/Scripts/UI_Stuff/UI_WinnerPanel.cs
@@ -7,20 +7,21 @@ public class UI_WinnerPanel : MonoBehaviour
 {
     public TextMeshProUGUI playerText, finalCoins;
     public GameObject winnerPirate, winnerPirateIcon;
+    private bool player1, player2;  //just to get rid of errors
 
     private void Start()
     {
-        if () //player 1 is the winner
+        if (player1) //player 1 is the winner
         {
             playerText.text = "Player 1";
-            finalCoins.text = ;//coins gained by player 1
+            finalCoins.text = "";//coins gained by player 1
             winnerPirateIcon.transform.GetChild(0).gameObject.SetActive(true);
             winnerPirateIcon.transform.GetChild(1).gameObject.SetActive(false);
         }
-        else if () //player 2 is the winner
+        else if (player2) //player 2 is the winner
         {
             playerText.text = "Player 2";
-            finalCoins.text = ;//coins gained by player 2
+            finalCoins.text = "";//coins gained by player 2
             winnerPirateIcon.transform.GetChild(1).gameObject.SetActive(true);
             winnerPirateIcon.transform.GetChild(0).gameObject.SetActive(false);
         }
